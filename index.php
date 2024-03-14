13c6b21dd95152c15a9f948a607c7c79067099aa
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-14 18:49:22 +0700

    creating new section (not finished)

--- index.php
+++ index.php
@@ -108,6 +108,45 @@
         <?php include("project_card.php")  ?>
     <!--EXPERIENCE END-->
 
+    <!-- DATA PROJECTS START -->
+    <div class="data-section section"  id="data">
+        <div class="content content-data">
+            <div class="sub">
+                <h3>04. Data Project</h>
+            </div>
+
+            <div class="flexbox-container flexbox-data">
+
+                <?php for ($i=0; $i < 6; $i++) : ?>
+                    <a href="http://">
+                    <div class="flexbox-item flexbox-item-data">
+                    
+                        <div class="flexbox-atasbawah">
+                            <h4>Investigating  Netflix Movie around the world Value</h4>
+                        </div>
+
+                        <div class="flexbox-atasbawah">
+                            <div class="project-tag">
+                                <ul>
+                                    <li>AI</li>
+                                    <li>Python</li>
+                                    <li>SNS</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    
+                    </div>
+                    </a>
+
+                <?php endfor;?>
+            </div>
+
+        </div>
+    </div>
+    <!-- DATA PROJECTS END -->
+    
+
     <!--CONTACT START-->
     <div class="contact-section section" id="contact">
         <div class="content content-contact">
