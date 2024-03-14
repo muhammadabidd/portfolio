75acb6de2d4ec8478d10fd0c18e995715d08feda
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-14 08:48:32 +0700

    connected json with php

--- test2.php
+++ test2.php
@@ -4,17 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <?php  $myfile2 = file_get_contents("json2.json");
+
+            $myfile2 = json_decode($myfile2);    ?>
+    
+
 </head>
 <body>
     
+<?php foreach ($myfile2 as $item):  ?>
+    <?php if(isset($item) && !empty($item)): ?>
 
-    <h1>This is the Title/ THis is going to be the name </h1>
+        <h1>  Hello Everyone, My Name is : <?php  echo $item->name ?> </h1>
 
-    <p style="color: blue;">This is  text 1 (age)</p>
+        <p style="color: blue;">I am <?php echo $item->age?> years old</p>
 
-    <p style="color: red">This is text 2 (major)</p>
+        <p style="color: red">and I am Majoring in <?php echo $item->major?> </p>
 
-    <p style="color: green">List of Courses</p>
+        <p style="color: green">This is the list of my courses : </p>
 
+
+        <ol>
+        <?php foreach ($item->courses as $course): ?>
+
+            <li><?php echo "$course" ?></li>
+
+        <?php endforeach; ?>
+        </ol>
+
+    <?php endif;?>
+<?php endforeach; ?>
 </body>
 </html>
