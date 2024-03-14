929d4d6d4eb5a1492988d55c5c7912f2c20b8b5d
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-14 08:52:12 +0700

    Got to go to real data

--- test2.php
+++ test2.php
@@ -16,22 +16,33 @@
 <?php foreach ($myfile2 as $item):  ?>
     <?php if(isset($item) && !empty($item)): ?>
 
+        <?php if(isset($item->name)): ?>
         <h1>  Hello Everyone, My Name is : <?php  echo $item->name ?> </h1>
+        <?php endif;?>
 
+
+        <?php if(isset($item->age)): ?>
         <p style="color: blue;">I am <?php echo $item->age?> years old</p>
+        <?php endif;?>
 
+
+        <?php if(isset($item->major)): ?>
         <p style="color: red">and I am Majoring in <?php echo $item->major?> </p>
+        <?php endif;?>
 
-        <p style="color: green">This is the list of my courses : </p>
+
+        <h3 style="color: green">This is the list of my courses : </h3>
 
 
         <ol>
+        <?php if(isset($item->courses)): ?>
         <?php foreach ($item->courses as $course): ?>
 
             <li><?php echo "$course" ?></li>
 
         <?php endforeach; ?>
         </ol>
+        <?php endif;?>
 
     <?php endif;?>
 <?php endforeach; ?>
