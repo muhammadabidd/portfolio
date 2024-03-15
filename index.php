818d5983b380244066a25b5d9b26d7817e6db7d1
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-15 10:57:56 +0700

    adding certification section

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
     <link rel="stylesheet" href="style/06SocialMediaRight.css">
     <link rel="stylesheet" href="style/animation.css">
     <link rel="stylesheet" href="style/07videosection.css">
+    <link rel="stylesheet" href="style/08certificationsection.css">
     </head>
 <body>
 
@@ -105,9 +106,56 @@
         <?php include("experience.php")  ?>
     <!--EXPERIENCE END-->
 
-    <!--EXPERIENCE START-->
+
+     <!-- CERTIFICATION START -->
+     <div class="certification-section section"  id="certification">
+        <div class="content content-certification">
+            <div class="sub">
+                <h3>03. My Certification</h>
+            </div>
+
+            <div class="flexbox-container flexbox-certification">
+
+                <?php for ($i=0; $i < 3; $i++) : ?>
+                    <a href="http://">
+                    <div class="flexbox-item flexbox-item-data">
+                    
+                        <div class="flexbox-atasbawah flexbox-atas">
+                            <div class="kiri">
+                                <img src="img/certification/data_analyst_associate_badge.png" alt="">
+                            </div>
+
+                            <div class="kanan">
+                                <h4>Investigating  Netflix Movie around the world Value</h4>
+                                <p>Nov 2022</p>
+                            </div>
+                            
+                        </div>
+
+                        <div class="flexbox-atasbawah">
+                            <div class="project-tag">
+                                <ul>
+                                    <li>AI</li>
+                                    <li>Python</li>
+                                    <li>SNS</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    
+                    </div>
+                    </a>
+
+                <?php endfor;?>
+            </div>
+
+        </div>
+    </div>
+    <!-- CERTIFICATION END -->
+
+    <!--PROJECT START-->
         <?php include("project_card.php")  ?>
-    <!--EXPERIENCE END-->
+    <!--PROJECT END-->
 
     <!-- DATA PROJECTS START -->
     <div class="data-section section"  id="data">
