c9dea7e062e5fd45cf0d5f7991306db08c6b5a09
Muhammad Abid <muhammmadabidd@gmail.com>
2024-03-15 10:25:45 +0700

    add the video section

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
     <link rel="stylesheet" href="style/06SocialMediaLeft.css">
     <link rel="stylesheet" href="style/06SocialMediaRight.css">
     <link rel="stylesheet" href="style/animation.css">
+    <link rel="stylesheet" href="style/07videosection.css">
     </head>
 <body>
 
@@ -150,7 +151,7 @@
         <div class="courses-section section"  id="courses">
         <div class="content content-courses">
             <div class="sub">
-                <h3>05. Courses Taken</h>
+                <h3>06. Courses Taken</h>
             </div>
 
             <div class="flexbox-container flexbox-courses">
@@ -182,7 +183,48 @@
 
         </div>
     </div>
-    <!-- COURSES END -->
+    <!-- COURSES TAKEN END -->
+
+    <!-- VIDEO START -->
+    <div class="video-section section"  id="video">
+        <div class="content content-video">
+            <div class="sub">
+                <h3>05. Video Lesson</h>
+            </div>
+
+            <div class="flexbox-container flexbox-video">
+
+                <?php for ($i=0; $i < 6; $i++) : ?>
+                    <a href="http://">
+                    <div class="flexbox-item flexbox-item-data">
+                    
+                        <div class="flexbox-atasbawah">
+                            <img src="img/thumbnail/01.webp" alt="">
+                            <h4>Investigating  Netflix Movie around the world Value</h4>
+                            <p>02 Nov 2024</p>
+                        </div>
+
+                        <div class="flexbox-atasbawah">
+                            <div class="project-tag">
+                                <ul>
+                                    <li>AI</li>
+                                    <li>Python</li>
+                                    <li>SNS</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    
+                    </div>
+                    </a>
+
+                <?php endfor;?>
+            </div>
+
+        </div>
+    </div>
+    <!-- VIDEO END -->
+
     
 
     <!--CONTACT START-->
